e06633684d608072bec2bead3522c99ae0ba06bd
MathewRo <rohitpmathew@yahoo.co.in>
2021-06-24 00:16:58 +0530

    changed indentation for arrays

--- containers/includes/array.hpp
+++ containers/includes/array.hpp
@@ -30,95 +30,118 @@ namespace miniSTL {
            though it may work without any crash */
 
         /* Modifiers */
-        void fill(const value_type& val) {
-          for (int i = 0; i < _size; i++) {
+        void fill(const value_type& val) 
+        {
+          for (int i = 0; i < _size; i++) 
+          {
             buffer[i] = val;
           }
         }
 
-        void swap (array& x) noexcept {
+        void swap (array& x) noexcept 
+        {
           /* This function should not throw exceptions */
-          for (int i = 0; i < (_size < x.size()?_size:x.size()); i++) {
+          for (int i = 0; i < (_size < x.size()?_size:x.size()); i++) 
+          {
             buffer[i] = x[i];
           }
         }
 
         /* Element access */
-        reference at(size_t n) {
+        reference at(size_t n) 
+        {
           return buffer[n];
         }
 
-        const_reference at(size_t n) const {
+        const_reference at(size_t n) const 
+        {
           return buffer[n];
         }
 
-        reference operator[](size_t n) {
+        reference operator[](size_t n) 
+        {
           return buffer[n];
         }
 
-        const_reference operator[](size_t n) const {
+        const_reference operator[](size_t n) const 
+        {
           return buffer[n]; 
         }
 
-        reference front() {
+        reference front() 
+        {
           return buffer[0];
         }
 
-        const_reference front() const{
+        const_reference front() const
+        {
           return buffer[0];
         }
 
-        reference back() {
+        reference back() 
+        {
           return buffer[_size-1];
         }
 
-        const_reference back() const{
+        const_reference back() const
+        {
           return buffer[_size-1];
         }
 
-        value_type* data() noexcept {
+        value_type* data() noexcept 
+        {
           return (pointer)&buffer[0];
         }
 
-        const value_type* data() const noexcept {
+        const value_type* data() const noexcept 
+        {
           return (const_pointer)&buffer[0]; 
         }
 
         /* capacity */
-        constexpr size_type size() const noexcept {
+        constexpr size_type size() const noexcept 
+        {
           return _size;
         }
 
-        constexpr size_type max_size() const noexcept {
+        constexpr size_type max_size() const noexcept 
+        {
           return _size;
         }
 
-        constexpr bool empty() const noexcept{
+        constexpr bool empty() const noexcept
+        {
           return _size == 0;
         }
 
         /* iterators */
-        iterator begin() noexcept{
+        iterator begin() noexcept
+        {
           return &buffer[0];
         }
 
-        const_iterator begin() const noexcept{
+        const_iterator begin() const noexcept
+        {
           return (const_iterator)&buffer[0];
         }
 
-        const_iterator cbegin() const noexcept{
+        const_iterator cbegin() const noexcept
+        {
           return &buffer[0];
         }
 
-        iterator end() noexcept{
+        iterator end() noexcept
+        {
           return &buffer[_size];
         }
 
-        const_iterator end() const noexcept{
+        const_iterator end() const noexcept
+        {
           return (const_iterator)&buffer[_size];
         }
 
-        const_iterator cend() const noexcept{
+        const_iterator cend() const noexcept
+        {
           return &buffer[_size];
         }
 
@@ -129,32 +152,38 @@ namespace miniSTL {
 
   /* Relational operator implementation */
   template <class T, size_t N>
-    bool operator== ( const array<T,N>& lhs, const array<T,N>& rhs ) {
+    bool operator== ( const array<T,N>& lhs, const array<T,N>& rhs ) 
+    {
       return (lhs.size() == rhs.size() && 
           std::equal(lhs.begin(), lhs.end(), rhs.begin()));
     }
 
   template <class T, size_t N>
-    bool operator!= ( const array<T,N>& lhs, const array<T,N>& rhs ) {
+    bool operator!= ( const array<T,N>& lhs, const array<T,N>& rhs ) 
+    {
       return !(lhs == rhs);
     }	
 
   template <class T, size_t N>
-    bool operator<  ( const array<T,N>& lhs, const array<T,N>& rhs ) {
+    bool operator<  ( const array<T,N>& lhs, const array<T,N>& rhs ) 
+    {
       return (std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
     }	
 
   template <class T, size_t N>
-    bool operator> ( const array<T,N>& lhs, const array<T,N>& rhs ) {
+    bool operator> ( const array<T,N>& lhs, const array<T,N>& rhs ) 
+    {
       return (rhs < lhs);
     }
   template <class T, size_t N>
-    bool operator<= ( const array<T,N>& lhs, const array<T,N>& rhs ) {
+    bool operator<= ( const array<T,N>& lhs, const array<T,N>& rhs ) 
+    {
       return !(rhs < lhs);
     }
 
   template <class T, size_t N>
-    bool operator>= ( const array<T,N>& lhs, const array<T,N>& rhs ) {
+    bool operator>= ( const array<T,N>& lhs, const array<T,N>& rhs ) 
+    {
       return !(lhs < rhs);
     }
 }
